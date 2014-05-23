e4456a5beae11907a5e613cce968040c08eed2c7
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2014-05-23 19:40:35 +0800

    Add fetch-stream-start, fetch-stream-end and lots of tokens

--- src/scanner.lisp
+++ src/scanner.lisp
@@ -36,18 +36,39 @@
   ((%indent :type fixnum
             :initform -1
             :accessor indent)
+   (%indents :type (list fixnum)
+             :initform ()
+             :accessor indents)
    (%simple-key-allowed-p :type boolean
                           :initform t
                           :accessor simple-key-allowed-p)
    (%stream-start-produced-p :type boolean
                              :initform nil
                              :accessor stream-start-produced-p)
+   (%flow-level-p :type boolean
+                  :initform nil
+                  :accessor flow-level-p)
    (%simple-keys :type (list simple-key)
                  :initform ()
                  :accessor simple-keys)
    (%tokens :type (list token)
             :initform ()
-            :accessor tokens)))
+            :accessor tokens)
+   (%mark   :type mark
+            :initform (make-mark)
+            :accessor position-mark)))
+
+(defun current-column (scanner)
+  (mark-column (position-mark scanner)))
+
+(defsetf current-column (scanner) (column)
+  `(setf (mark-column (position-mark ,scanner)) ,column))
+
+(defun current-line (scanner)
+  (mark-line (position-mark scanner)))
+
+(defsetf current-line (scanner) (line)
+  `(setf (mark-line (position-mark ,scanner)) ,line))
 
 (defun make-scanner ()
   (make-instance 'scanner))
@@ -55,13 +76,20 @@
 (defclass mark ()
   ((%index  :type fixnum
             :initform 0
-            :reader mark-index)
+            :accessor mark-index)
    (%line   :type fixnum
             :initform 0
-            :reader mark-line)
+            :accessor mark-line)
    (%column :type fixnum
             :initform 0
-            :reader mark-column)))
+            :accessor mark-column)))
+
+(defmethod print-object ((mark mark) stream)
+  (print-unreadable-object (mark stream :type t)
+    (format stream "index: ~a line: ~a column: ~a"
+            (mark-index mark)
+            (mark-line mark)
+            (mark-column mark))))
 
 (defun make-mark ()
   (make-instance 'mark))
@@ -69,7 +97,7 @@
 (defclass simple-key ()
   ((%possible :type boolean
               :initform nil
-              :reader simple-key-possible-p)
+              :accessor simple-key-possible-p)
    (%required :type boolean
               :initform nil
               :reader simple-key-required-p)
@@ -85,31 +113,128 @@
 
 (defclass token ()
   ((%start-mark :type mark
+                :initarg :start
                 :reader token-start)
    (%end-mark   :type mark
+                :initarg :end
                 :reader token-end)))
+
+(defgeneric print-token-data (token stream)
+  (:documentation "Print token's data")
+  (:method (token stream)))
+
+(defmethod print-object ((token token) stream)
+  (print-unreadable-object (token stream :type t)
+    (print-token-data token stream)))
+
+(defun make-token (token-type start-mark end-mark)
+  (make-instance token-type :start start-mark :end end-mark))
+
+(defclass no (token) ())
 
 (deftype encoding ()
   '(member :any :utf-8 :utf-16-le :utf-16-be))
 
-(defclass stream-start-token (token)
+(defclass stream-start (token)
   ((%encoding :type encoding
               :initform :any
-              :reader stream-start-token-encoding)))
+              :initarg :encoding
+              :reader stream-start-encoding)))
 
-(defun make-stream-start-token ()
-  (make-instance 'stream-start-token))
+(defun make-stream-start (encoding start-mark end-mark)
+  (make-instance 'stream-start :encoding encoding
+                               :start start-mark
+                               :end end-mark))
 
-(defmethod print-object ((token stream-start-token) stream)
-  (print-unreadable-object (token stream :type t)
-    (princ (stream-start-token-encoding token) stream)))
+(defmethod print-token-data ((token stream-start) stream)
+  (princ (stream-start-encoding token) stream))
+
+(defclass stream-end (token) ())
+
+(defun make-stream-end (start-mark end-mark)
+  (make-token 'stream-end start-mark end-mark))
+
+(defclass version-directive (token)
+  ((%major :type fixnum
+           :reader version-major)
+   (%minor :type fixnum
+           :reader version-minor)))
+(defclass tag-directive (token)
+  ((%handle :type string
+            :reader tag-handle)
+   (%suffix :type string
+            :reader tag-suffix)))
+(defclass document-start (token) ())
+(defclass document-end (token) ())
+(defclass block-sequence-start (token) ())
+(defclass block-mapping-start (token) ())
+(defclass block-end (token) ())
+(defclass flow-sequence-start (token) ())
+(defclass flow-sequence-end (token) ())
+(defclass flow-mapping-start (token) ())
+(defclass flow-mapping-end (token) ())
+(defclass block-entry (token) ())
+(defclass flow-entry (token) ())
+(defclass key (token) ())
+(defclass value (token) ())
+(defclass alias (token)
+  ((%value :type string
+           :reader token-value)))
+(defclass anchor (token)
+  ((%value :type string
+           :reader token-value)))
+(defclass tag (token)
+  ((%handle :type string
+            :reader tag-handle)
+   (%suffix :type string
+            :reader tag-suffix)))
+(defclass scalar (token)
+  ((%value :type string
+           :reader token-value)
+   (%length :type fixnum
+            :reader scalar-length)
+   (%style :type scalar-style
+           :reader scalar-style)))
+
+(defun unroll-indent (scanner column)
+  (unless (flow-level-p scanner)
+    (loop :while (> (indent scanner) column)
+          :do (push (make-token 'block-end
+                                (position-mark scanner)
+                                (position-mark scanner))
+                    (tokens scanner))
+              (setf (indent scanner) (pop (indents scanner))))))
+
+(defun remove-simple-key (scanner)
+  (let ((simple-key (first (simple-keys scanner))))
+    (when (simple-key-possible-p simple-key)
+      (when (simple-key-required-p simple-key)
+        (error "~a While scanning a simple key, could not find expected ':'"
+               (position-mark scanner))))
+    (setf (simple-key-possible-p simple-key) nil)))
 
 (defun fetch-stream-start (scanner)
   (setf (indent scanner) -1
         (simple-key-allowed-p scanner) t
         (stream-start-produced-p scanner) t)
   (push (make-simple-key) (simple-keys scanner))
-  (push (make-stream-start-token) (tokens scanner)))
+  (push (make-stream-start :any
+                           (position-mark scanner)
+                           (position-mark scanner))
+        (tokens scanner))
+  (values))
+
+(defun fetch-stream-end (scanner)
+  (when (not (zerop (current-column scanner)))
+    (setf (current-column scanner) 0)
+    (incf (current-line scanner)))
+  (unroll-indent scanner -1)
+  (remove-simple-key scanner)
+  (setf (simple-key-allowed-p scanner) nil)
+  (push (make-stream-end (position-mark scanner)
+                         (position-mark scanner))
+        (tokens scanner))
+  (values))
 
 ;;; scanner.lisp ends here
 
